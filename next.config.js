8cd809501a585d8d6940acd9dfaba4a756b5914b
Frank14b <franckfontcha@gmail.com>
2022-08-27 23:01:25 +0400

    update home page

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextTranslate = require('next-translate');
 
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false, stream: false, constants: false };
     return config;
